0ea62654af193f914e4abd8f0ecec43a524f2176
Ttst <thibautstachnick@gmail.com>
2024-01-02 10:59:08 +0100

    add the default template for groups and add the method getAllGroupsFromActualUser in the component

--- src/app/group-manager/group-manager.component.ts
+++ src/app/group-manager/group-manager.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+import {Group} from "../group";
+import {GroupService} from "../group.service";
+import {Message} from "../message";
+import {UserFull} from "../user-full";
+import {ProfileFull} from "../profile-full";
+import {GlobalConstants} from "../common/global-constants";
 
 @Component({
   selector: 'app-group-manager',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './group-manager.component.html',
   styleUrl: './group-manager.component.css'
 })
 export class GroupManagerComponent {
 
+  groups: Group[] = []
+  groupService: GroupService = inject(GroupService)
+
+  constructor() {
+    this.getAllGroupsFromActualUser()
+  }
+
+
+  getAllGroupsFromActualUser(){
+    this.groupService.getAllGroupsFromActualUser().subscribe({
+      next:(groupsFromFetch:any)=>{
+        for (let i =0;i<groupsFromFetch.length;i++){
+          let newGroup :Group = {
+            groupMessages: groupsFromFetch[i].groupMessages,
+            groupMembers: groupsFromFetch[i].groupMembers,
+            adminMembers: groupsFromFetch[i].adminMembers,
+            owner: groupsFromFetch[i].owner
+          }
+          this.groups.push(newGroup)
+        }
+      }
+    })
+  }
+
+
+  protected readonly GlobalConstants = GlobalConstants;
 }
