23d7e981c4ea58c40ff5dd4b2f4d7391db688e69
Ttst <thibautstachnick@gmail.com>
2024-01-02 19:42:55 +0100

    add a rule when url is registering a user in order to avoid token association on null

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -3,6 +3,9 @@ import {GlobalConstants} from "../common/global-constants";
 
 export const spinnerInterceptor: HttpInterceptorFn = (req, next) => {
 
+  if (req.url == "https://slackbis.thibautstachnick.com/api/register"){
+    return next(req)
+  }else
     return next(req.clone({
         setHeaders: {
             Authorization: `Bearer ${GlobalConstants.token}`
