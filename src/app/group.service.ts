725052774b62aa9451af6d1fe92bfb1f8108dca3
Ttst <thibautstachnick@gmail.com>
2024-01-02 10:44:43 +0100

    add function to geta ll groups from actual user

--- src/app/group.service.ts
+++ src/app/group.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {GlobalConstants} from "./common/global-constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+
+  getAllGroupsFromActualUser(){
+    return this.http.get(GlobalConstants.baseUrl+"group/conversation/showAll")
+  }
+
 }
