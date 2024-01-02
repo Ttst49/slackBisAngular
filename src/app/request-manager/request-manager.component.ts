2f23065b682fedff6f9cb2a31e23fced8e1ebd78
Ttst <thibautstachnick@gmail.com>
2024-01-02 21:29:28 +0100

    add a request manager

---
+++ src/app/request-manager/request-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-manager.component.html',
+  styleUrl: './request-manager.component.css'
+})
+export class RequestManagerComponent {
+
+}
