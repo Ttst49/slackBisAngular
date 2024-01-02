d73d2341b7e13a9847990dc4dab881b3dda04540
Ttst <thibautstachnick@gmail.com>
2024-01-02 19:42:11 +0100

    make group-detail available with new message service

--- src/app/group-detail/group-detail.component.ts
+++ src/app/group-detail/group-detail.component.ts
@@ -5,15 +5,20 @@ import {GroupService} from "../group.service";
 import {Message} from "../message";
 import {UserFull} from "../user-full";
 import {ProfileFull} from "../profile-full";
-import {NgIf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {GlobalConstants} from "../common/global-constants";
+import {FormsModule} from "@angular/forms";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-group-detail',
   standalone: true,
   imports: [
     NgIf,
-    RouterLink
+    RouterLink,
+    FormsModule,
+    NgForOf,
+    NgClass
   ],
   templateUrl: './group-detail.component.html',
   styleUrl: './group-detail.component.css'
@@ -23,17 +28,22 @@ export class GroupDetailComponent {
   group !: Group
   route : ActivatedRoute = inject(ActivatedRoute)
   groupService: GroupService = inject(GroupService)
+  messageService: MessageService = inject(MessageService)
+  messageInput: string = ""
+
 
   constructor() {
     if (this.route.snapshot.url[2]){
       let id : number = parseInt(this.route.snapshot.url[2].path)
       this.getOneGroupFromId(id)
     }
+
   }
 
   getOneGroupFromId(id:number){
     this.groupService.getOneGroupFromId(id).subscribe({
       next:(groupFromFetch:any)=>{
+        console.log(groupFromFetch.groupMembers)
         return this.group = {
           id: groupFromFetch.id,
           groupMessages: groupFromFetch.groupMessages,
@@ -44,6 +54,7 @@ export class GroupDetailComponent {
       }
     })
   }
+
 
   protected readonly GlobalConstants = GlobalConstants;
 }
