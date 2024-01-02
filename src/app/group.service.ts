9d1678c6994812dc0b74b8ab2dd8bb286d77ea6f
Ttst <thibautstachnick@gmail.com>
2024-01-02 17:01:40 +0100

    add route for group creation and add it needed data

--- src/app/group.service.ts
+++ src/app/group.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {GlobalConstants} from "./common/global-constants";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient, private router: Router) { }
 
 
   getAllGroupsFromActualUser(){
@@ -16,5 +17,9 @@ export class GroupService {
 
   getOneGroupFromId(id: number) {
     return this.http.get(GlobalConstants.baseUrl+`group/conversation/show/${id}`)
+  }
+
+  createGroup(selected_values: number[]) {
+    return this.http.post(GlobalConstants.baseUrl+"group/conversation/create",{members: selected_values})
   }
 }
