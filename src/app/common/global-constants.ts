6f54c1d6730e6e64cd9c39ca310cdf76bbe38c26
Ttst <thibautstachnick@gmail.com>
2024-01-02 23:26:48 +0100

    adding a global constant profile

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,4 +1,5 @@
 import {UserFull} from "../user-full";
+import {ProfileFull} from "../profile-full";
 
 
 export class GlobalConstants{
@@ -7,6 +8,7 @@ export class GlobalConstants{
   public static token = localStorage.getItem("bearerToken")
   public static baseUrl: string = "https://slackbis.thibautstachnick.com/api/"
   public static actualUser : UserFull
+  public static actualProfile: ProfileFull
   public static errorStatus : string
 
 }
