e9d63328bcdd78992335148123aee4ba40ec7f02
Ttst <thibautstachnick@gmail.com>
2024-01-02 21:53:50 +0100

    add the method to get all requests from Request service

--- src/app/request-manager/request-manager.component.ts
+++ src/app/request-manager/request-manager.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+import {GlobalConstants} from "../common/global-constants";
+import {RequestService} from "../request.service";
 
 @Component({
   selector: 'app-request-manager',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './request-manager.component.html',
   styleUrl: './request-manager.component.css'
 })
 export class RequestManagerComponent {
 
+  requestService: RequestService = inject(RequestService)
+
+  constructor() {
+    this.getAllRequests()
+  }
+
+  getAllRequests(){
+    this.requestService.getAllRequests().subscribe({
+      next:(requestsFromFetch)=>{
+        console.log(requestsFromFetch)
+      }
+    })
+  }
+
+
+  protected readonly GlobalConstants = GlobalConstants;
 }
