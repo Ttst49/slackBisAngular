d5beae02dca8268b152042ed0a66f110e59f5397
Ttst <thibautstachnick@gmail.com>
2024-03-31 16:37:21 +0200

    add conversation manager component to app

---
+++ src/app/conversation-manager/conversation-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversation-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './conversation-manager.component.html',
+  styleUrl: './conversation-manager.component.css'
+})
+export class ConversationManagerComponent {
+
+}
