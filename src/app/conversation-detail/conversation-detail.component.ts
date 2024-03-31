42fa7f81d5f571a464a618977023efc119e23d6f
Ttst <thibautstachnick@gmail.com>
2024-03-31 16:37:30 +0200

    add conversation detail component to app

---
+++ src/app/conversation-detail/conversation-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversation-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './conversation-detail.component.html',
+  styleUrl: './conversation-detail.component.css'
+})
+export class ConversationDetailComponent {
+
+}
