e1abbd50bd629bee171cfb7668c30e9590f278fc
Ttst <thibautstachnick@gmail.com>
2024-04-11 11:55:25 +0200

    add a way to get a conversation from a service

--- src/app/conversation.service.ts
+++ src/app/conversation.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {GlobalConstants} from "./common/global-constants";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {Message} from "./message";
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,16 @@ export class ConversationService {
     return this.http.get(GlobalConstants.baseUrl+"private/conversation/showConversations")
   }
 
+
+  sendMessage(conversationId: number, message: Message) {
+    this.http.post(GlobalConstants.baseUrl+`private/message/send/${conversationId}`,message).subscribe({
+      next:(data)=>{
+        console.log(data)
+      }
+    })
+  }
+
+  getOneConversationFromId(id: number) {
+    return this.http.get(GlobalConstants.baseUrl+`private/conversation/show/${id}`)
+  }
 }
